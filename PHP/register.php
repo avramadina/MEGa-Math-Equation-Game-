448d7842b59f1466ec7393fd350f3326333f1cb7
iuliaoceanu <79100680+iuliaoceanu@users.noreply.github.com>
2021-05-26 19:23:44 +0300

    Create register.php

---
+++ PHP/register.php
@@ -0,0 +1,38 @@
+<?php
+
+    class Utilizator{ // CRUD = Create / Read / Update / Delete
+     
+        public function adauga_utilizator(){
+            $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=iulia"); 
+
+            if(isset($_POST["nume"]) && $_POST["nume"]!='' &&
+                isset($_POST["prenume"]) && $_POST["prenume"]!='' &&
+                isset($_POST["email"]) && $_POST["email"]!='' &&
+                isset($_POST["parola"]) && $_POST["parola"]!='') {
+
+                $nume = $_POST["nume"];
+                $prenume = $_POST["prenume"];
+                $email = $_POST["email"];
+                $parola = md5($_POST["parola"]);
+            
+                $logat = false;
+
+                $query = pg_query($conn, "INSERT INTO users (id, nume, prenume, email, parola, logat)  VALUES ((SELECT COALESCE(MAX(id)+1,1) FROM users), '$nume', '$prenume', '$email', '$parola', '$logat');");
+                if ( $query ) {
+                    header('Location: Login.html');
+                    die();
+                }
+                else echo "Nu m am conecat";
+            
+            } 
+            else echo "Nu m am conectat";  
+            
+        }
+    }
+
+    
+    $utilizator = new Utilizator();
+    $utilizator->adauga_utilizator();
+   
+
+?>
