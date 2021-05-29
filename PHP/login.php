cdf8078ac1a56e70b663a4061be4bd752608c7ee
alexandrapohoata <79059482+alexandrapohoata@users.noreply.github.com>
2021-05-29 20:18:40 +0300

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -1,34 +1,38 @@
 <?php
-// class Login{
-        // public function autentificare(){
+class Login
+{
+    public function autentificare()
+    {
+        $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=postgres");
+        session_start();
+        if (isset($_POST["email"]) && isset($_POST["parola"])) {
+            $email = $_POST["email"];
+            $parola = md5($_POST["parola"]);
 
-            $conn = pg_connect("host=localhost port=5432 dbname=postgres user=postgres password=iulia"); 
-            session_start();
-            if(isset($_POST["email"]) && $_POST["email"]!='' &&
-                isset($_POST["parola"]) && $_POST["parola"]!='') {
+            $query = "SELECT * FROM users WHERE email = '" . $email . "' AND parola = '" . $parola . "' ;";
+            $result = pg_query($conn, $query);
 
-                $email = $_POST["email"];
-                $parola = md5($_POST["parola"]);
-            
-                $query = "SELECT * FROM users WHERE email = $1 AND parola = $2;";
-                $result = pg_query_params($conn, $query, array($email, $parola));
-                if(pg_num_rows($result) > 0) 
-                    $_SESSION["utilizator"] = $_POST["email"];
-                    header('Location: ../PHP/indexAdmin.php');
-                    die();
+            if (pg_num_rows($result) > 0) {
+                $_SESSION["utilizator"] = $_POST["email"];
+                return true;
             }
-                
-            //      else echo "Nu m am conectat"; 
-            
-            // } 
-            // else echo "Nu m am conectat";  
+        }
+        return false;
+    }
+}
 
-        //}
+$utilizator = new Login();
 
-    // }
-
-    // $utilizator = new Login();
-    // $utilizator->autentificare();
-
-
-    ?>
+if ($utilizator->autentificare()) {
+    if (isset($_SESSION["utilizator"])) {
+        // daca utilizatorul a folosit la logare email-ul "admin@yahoo.com", atunci merg in PHP/indexAdmin.php
+        if ($_SESSION["utilizator"] == "admin@yahoo.com")
+            header('Location: ../PHP/indexAdmin.php');
+        else // daca nu, il duc in PHP/indexGame.php
+            header('Location: ../PHP/indexGame.php');
+    }
+} else {
+    // header('Location: ../HTML/Login.html');
+    echo "<script>alert('Logarea a esuat. Incearca din nou.');</script>";
+}
+?>
